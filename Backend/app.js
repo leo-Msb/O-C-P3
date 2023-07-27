a72bfdbd73c46c10ce4bc8bfe7b4eded76e075b0
leo-Msb <msbauto.d@gmail.com>
2023-07-27 10:11:49 +0200

    Add pakage

--- Backend/app.js
+++ Backend/app.js
@@ -1,27 +1,43 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors')
-require('dotenv').config();
-const helmet = require('helmet');
-const swaggerUi = require('swagger-ui-express')
-const yaml = require('yamljs')
-const swaggerDocs = yaml.load('swagger.yaml')
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(helmet({
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
+const helmet = require("helmet");
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yamljs");
+const swaggerDocs = yaml.load("swagger.yaml");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.options("*", cors());
+app.use(
+  helmet({
     crossOriginResourcePolicy: false,
-  }));
-app.use('/images', express.static(path.join(__dirname, 'images')))
+  })
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.get((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 const db = require("./models");
-const userRoutes = require('./routes/user.routes');
-const categoriesRoutes = require('./routes/categories.routes');
-const worksRoutes = require('./routes/works.routes');
-db.sequelize.sync().then(()=> console.log('db is ready'));
-app.use('/api/users', userRoutes);
-app.use('/api/categories', categoriesRoutes);
-app.use('/api/works', worksRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+const userRoutes = require("./routes/user.routes");
+const categoriesRoutes = require("./routes/categories.routes");
+const worksRoutes = require("./routes/works.routes");
+db.sequelize.sync().then(() => console.log("db is ready"));
+app.use("/api/users", userRoutes);
+app.use("/api/categories", categoriesRoutes);
+app.use("/api/works", worksRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 module.exports = app;
